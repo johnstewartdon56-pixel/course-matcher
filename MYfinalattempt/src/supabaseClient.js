f0864bce0589dd7ba0ba7d624e8ad6c5596c233d
johnstewartdon56-pixel <johnstewartdon56@gmail.com>
2026-08-24 12:23:08 +0200

    Delete MYfinalattempt directory

--- MYfinalattempt/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://ybuawwogwxnhsgznqdui.supabase.co";
-const supabaseAnonKey = "sb_publishable_bmXKh3_fXcLrlJwgdsHi5w__fFSsjKk";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
